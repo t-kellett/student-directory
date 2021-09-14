b9358ae3dc273703f0be6af1f853e3e1f4ef1686
Tom <t.p.kellett@gmail.com>
2021-09-14 11:57:13 +0100

    Adding user input method to gather array of students

--- directory.rb
+++ directory.rb
@@ -1,17 +1,20 @@
-#adding students to an array
-students = [
-{name: "Dr. Hannibal Lecter", cohort: :september},
-{name: "Darth Vader", cohort: :september},
-{name: "Nurse Ratched", cohort: :september},
-{name: "Michael Corleone",  cohort: :september},
-{name: "Alex DeLarge",  cohort: :september},
-{name: "The Wicked Witch of the West", cohort: :september},
-{name: "Terminator", cohort: :september},
-{name: "Freddy Kreuger", cohort: :september},
-{name: "The Joker", cohort: :september},
-{name: "Joffrey Baratheon", cohort: :september},
-{name: "Norman Bates", cohort: :september}
-]
+#input gathering student array
+def input_students
+  puts "Please enter a names of the students"
+  puts "To finish, just hit return twice"
+  #empty array for students
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :september}
+    puts "Now we have #{students.count} students"
+    #get another name
+    name = gets.chomp
+  end
+  #return array of students
+  students
+end 
+
 #printing them out
 def print_header
   puts "The students of Villains Academy"
@@ -28,6 +31,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = input_students
 print_header()
 print(students)
 print_footer(students)
