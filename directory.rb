350752bacde0451b56ef3afcb6bbfc8589df4c16
Tom <t.p.kellett@gmail.com>
2021-09-14 13:54:34 +0100

    Adding interactive menu and asscociated methods

--- directory.rb
+++ directory.rb
@@ -1,18 +1,15 @@
+@students = []
 #input gathering student array
 def input_students
   puts "Please enter a names of the students"
   puts "To finish, just hit return twice"
-  #empty array for students
-  students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :september}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :september}
+    puts "Now we have #{@students.count} students"
     #get another name
     name = gets.chomp
   end
-  #return array of students
-  students
 end 
 
 #printing them out
@@ -21,17 +18,48 @@ def print_header
   puts "------------"
 end
 
-def print(students) 
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def print_student_list() 
+  @students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer()
+  puts "Overall, we have #{@students.count} great students"
 end
 
-students = input_students
-print_header()
-print(students)
-print_footer(students)
+def print_menu
+  puts "Please enter a number from the options below (enter as an integer)"
+  puts "1 - Input the students"
+  puts "2 - Show the students"
+  puts "9 - Exit"
+end
+
+def show_students
+  print_header()
+  print_student_list()
+  print_footer()
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
+#interactive menu
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+interactive_menu()
